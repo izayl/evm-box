4487b18ed51df23c1c2617902707d81ce863bd1b
izayl <izayl@163.com>
2021-06-24 01:19:41 +0800

    fix: merge network

--- common/utils.ts
+++ common/utils.ts
@@ -2,5 +2,11 @@ export const mergeNetworkConfig = (
   initial: Chain[],
   custom: Array<AtLeastOne<Chain, 'chainId'>>,
 ): Chain[] => {
-  return Object.assign(initial, custom)
+  return initial.map(item => {
+    const customItem = custom.find(c => c.chainId === item.chainId)
+
+    if (customItem) return Object.assign({}, item, customItem)
+
+    return item
+  })
 }
