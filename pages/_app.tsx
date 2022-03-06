290eac489f2b19f1f942c780f4177c180157f1ec
izayl <izayl@163.com>
2022-03-06 22:17:36 +0800

    feat: add ga

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import { CssBaseline, GeistProvider } from '@geist-ui/react'
 import { AppProps } from 'next/app'
+import Script from 'next/script'
 import Head from 'next/head'
+import * as gtag from '../common/ga'
 import { LocaleProvider, useLocale } from '../common/hooks/useLocale'
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
@@ -15,20 +17,33 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
             name="viewport"
             content="width=device-width, initial-scale=1, user-scalable=no"
           />
-          <meta
-            name="description"
-            content={t('AppDesc')}
-          />
+          <meta name="description" content={t('AppDesc')} />
           <meta
             name="keywords"
             content="ETH,EVM,EVM Compatible Network,Network,Blockchain,chain"
           />
           <meta property="og:title" content="EVM Box" />
-          <meta
-            property="og:description"
-            content={t('AppDesc')}
-          />
+          <meta property="og:description" content={t('AppDesc')} />
         </Head>
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
         <Component {...pageProps} />
       </GeistProvider>
     </LocaleProvider>
