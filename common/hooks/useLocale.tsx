4b12dc6739352f0e8caa38164aa14e34ff2cd96e
izayl <izayl@163.com>
2022-05-22 18:21:35 +0800

    feat(i18n): add query support

--- common/hooks/useLocale.tsx
+++ common/hooks/useLocale.tsx
@@ -1,14 +1,20 @@
 import { createContext, useContext, useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
 import zh from '../locales/zh.json'
 import en from '../locales/en.json'
 
+enum Locale {
+  Zh = 'zh',
+  En = 'en',
+}
+
 export interface LocaleContextType {
-  locale: 'en' | 'zh'
+  locale: Locale
   langs: Record<'zh' | 'en', Record<string, string>>
 }
 
 export const LocaleContext = createContext<LocaleContextType>({
-  locale: 'en',
+  locale: Locale.En,
   langs: {
     zh,
     en,
@@ -24,14 +30,21 @@ export const useLocale = () => {
 
 export const LocaleProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [state, setState] = useState<LocaleContextType>({
-    locale: 'en',
+    locale: Locale.En,
     langs: { zh, en },
   })
+  const { query } = useRouter()
   useEffect(() => {
     const locale = navigator.language.split('-')[0]
 
-    setState({ ...state, locale: locale === 'zh' ? 'zh' : 'en' })
+    setState({ ...state, locale: locale === 'zh' ? Locale.Zh : Locale.En })
   }, [])
+
+  useEffect(() => {
+    if (query.lang && ['zh', 'en'].includes(query.lang as string)) {
+      setState({ ...state, locale: query.lang as Locale })
+    }
+  }, [query.lang])
 
   return (
     <LocaleContext.Provider value={state}>{children}</LocaleContext.Provider>
