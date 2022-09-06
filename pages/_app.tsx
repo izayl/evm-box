f386fc3bfa1bd50a8ee442bd0617f972a5d7ffbf
izayl <izayl@163.com>
2022-09-06 18:44:06 +0800

    fix: upgrade next to resolve Script issue

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,7 @@
 import { useEffect, useState } from 'react'
 import { CssBaseline, GeistProvider, useTheme } from '@geist-ui/react'
 import { AppProps } from 'next/app'
-import Script from 'next/script'
 import Head from 'next/head'
-import * as gtag from '../common/ga'
 import { LocaleProvider, useLocale } from '../common/hooks/useLocale'
 import { ThemeProvider } from '../common/hooks/useThemeContext'
 import useThemeDetector from '../common/hooks/useThemeDetector'
@@ -41,25 +39,9 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
             />
             <meta property="og:title" content="EVM Box" />
             <meta property="og:description" content={t('AppDesc')} />
-            <meta name="google-site-verification" content="aRVvcWYD39LeQ16swsN4aEXrOdrO2u2yegnNfoQeTVs" />
-            {/* Global Site Tag (gtag.js) - Google Analytics */}
-            <Script
-              strategy="afterInteractive"
-              src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-            />
-            <Script
-              id="gtag-init"
-              strategy="afterInteractive"
-              dangerouslySetInnerHTML={{
-                __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${gtag.GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-              }}
+            <meta
+              name="google-site-verification"
+              content="aRVvcWYD39LeQ16swsN4aEXrOdrO2u2yegnNfoQeTVs"
             />
           </Head>
           <Component {...pageProps} />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,8 @@ import Document, {
   Main,
   NextScript,
 } from 'next/document'
+import Script from 'next/script'
+import * as gtag from '../common/ga'
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -44,6 +46,24 @@ export default class MyDocument extends Document {
             href="/images/favicon-16x16.png"
           />
           <link rel="apple-touch-icon" href="/images/apple-touch-icon.png" />
+          <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+          />
+          <Script
+            id="gtag-init"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
         </Head>
         <body>
           <Main />
